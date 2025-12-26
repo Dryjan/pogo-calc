d779a5a14812fcfa9b32149abbca8eb5701e7c5e
Dryjan <145480200+Dryjan@users.noreply.github.com>
2025-12-26 16:27:38 +0100

    xd

---
+++ script.js
@@ -0,0 +1,25 @@
+let nn = 1;
+let wk = 1.6;
+let rs = 0.625;
+let dr = 0.390625;
+
+typesArr = [
+  [nn, wk, nn, nn, rs, rs, rs, rs, rs, wk, nn, nn, nn, rs, wk, nn, rs, nn],//Bug
+  [nn, rs, nn, nn, rs, rs, nn, nn, wk, nn, nn, nn, nn, nn, wk, nn, nn, nn],//Dark
+  [nn, nn, wk, nn, dr, nn, nn, nn, nn, nn, nn, nn, nn, nn, nn, nn, rs, nn],//Dragon
+  [nn, nn, rs, rs, nn, nn, nn, wk, nn, rs, dr, nn, nn, nn, nn, nn, nn, wk],//Electric
+  [nn, wk, wk, nn, nn, wk, rs, nn, nn, nn, nn, nn, nn, rs, nn, nn, rs, nn],//Fairy
+  [rs, wk, nn, nn, rs, nn, nn, rs, dr, nn, nn, wk, wk, rs, rs, wk, wk, nn],//Fighting
+  [wk, nn, rs, nn, nn, nn, rs, nn, nn, wk, nn, wk, nn, nn, nn, rs, wk, rs],//Fire
+  [wk, nn, nn, rs, nn, wk, nn, nn, nn, wk, nn, nn, nn, nn, nn, rs, rs, nn],//Flying
+  [nn, rs, nn, nn, nn, nn, nn, nn, wk, nn, nn, nn, dr, nn, wk, nn, nn, nn],//Ghost
+  [rs, nn, rs, nn, nn, nn, rs, rs, nn, rs, wk, nn, nn, rs, nn, wk, rs, wk],//Grass
+  [rs, nn, nn, wk, nn, nn, wk, dr, nn, rs, nn, nn, nn, wk, nn, wk, wk, nn],//Ground
+  [nn, nn, wk, nn, nn, nn, rs, wk, nn, wk, wk, rs, nn, nn, nn, nn, rs, rs],//Ice
+  [nn, nn, nn, nn, nn, nn, nn, nn, dr, nn, nn, nn, nn, nn, nn, rs, rs, nn],//Normal
+  [nn, nn, nn, nn, wk, nn, nn, nn, rs, wk, rs, nn, nn, rs, nn, rs, dr, nn],//Poison
+  [nn, dr, nn, nn, nn, wk, nn, nn, nn, nn, nn, nn, nn, wk, rs, nn, rs, nn],//Psychic
+  [wk, nn, nn, nn, nn, rs, wk, wk, nn, nn, rs, wk, nn, nn, nn, nn, rs, nn],//Rock
+  [nn, nn, nn, rs, wk, nn, rs, nn, nn, nn, nn, wk, nn, nn, nn, wk, rs, rs],//Steel
+  [nn, nn, rs, nn, nn, nn, wk, nn, nn, rs, wk, nn, nn, nn, nn, wk, nn, rs],//Water
+];
